9287b0653c7611f67fb6aa15b297996b42611221
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-03-30 22:43:29 -0300

    initial commit

---
+++ src/env/index.ts
@@ -0,0 +1,27 @@
+import { config } from 'dotenv'
+import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({
+    path: '.env.test',
+  })
+} else {
+  config()
+}
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
+})
+
+const parseResult = envSchema.safeParse(process.env)
+
+if (parseResult.success === false) {
+  console.error('invalid environment variables', parseResult.error.format())
+
+  throw new Error('invalid environment variables')
+}
+
+export const env = parseResult.data
