dead5b1e8935e48d7bff9af93c0d48f553070879
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-03-30 23:15:28 -0300

    Added user creation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import { usersRoutes } from './routes/users'
+
+const baseRoute = '/daily-diet'
 
 export const app = fastify()
+
+app.register(cookie)
+
+app.register(usersRoutes, {
+  prefix: `${baseRoute}/users`,
+})
---
+++ src/routes/users.ts
@@ -0,0 +1,32 @@
+import { randomUUID } from 'crypto'
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+import { z } from 'zod'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createUserSchema = z.object({
+      firstName: z.string(),
+      lastName: z.string(),
+      email: z.string().email(),
+    })
+
+    const { firstName, lastName, email } = createUserSchema.parse(request.body)
+
+    const userId = randomUUID()
+
+    await knex('users').insert({
+      id: userId,
+      first_name: firstName,
+      last_name: lastName,
+      email,
+    })
+
+    reply.cookie('session_id', userId, {
+      path: '/',
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    })
+
+    return reply.status(201).send()
+  })
+}
