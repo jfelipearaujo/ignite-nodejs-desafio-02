d6dbee9e936424910ed03de3eea6bb0adc1b858c
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-03-30 23:53:32 -0300

    add route to edit a meal

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 - [X] Should be possible to create a user
 - [X] Should be able to identify a user between requests
 - [X] Should be able to create a meal
-- [ ] Should be able to edit a meal
+- [X] Should be able to edit a meal
 - [ ] Should be able to delete a meal
 - [X] Should be able to view all user's meals
 - [X] Should be able to view only one meal
@@ -13,4 +13,4 @@
   - [ ] Total of meals 'on diet'
   - [ ] Total of meals 'off diet'
   - [ ] Best sequence of days 'on diet'
-- [ ] The use can only interact with it's own meals
+- [X] The use can only interact with it's own meals
--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -67,4 +67,46 @@ export async function mealsRoutes(app: FastifyInstance) {
 
     return reply.status(404).send()
   })
+
+  app.put('/:id', async (request, reply) => {
+    const { sessionId } = request.cookies
+
+    const getMealSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const updateMealSchema = z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.string().regex(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/),
+      time: z.string().regex(/^[0-9]{2}:[0-9]{2}$/),
+      isOnDiet: z.boolean(),
+    })
+
+    const { id } = getMealSchema.parse(request.params)
+
+    const meal = await knex('meals')
+      .where('user_id', sessionId)
+      .where('id', id)
+      .first()
+
+    if (!meal) {
+      return reply.status(404).send()
+    }
+
+    const { title, description, date, time, isOnDiet } = updateMealSchema.parse(
+      request.body,
+    )
+
+    await knex('meals').update({
+      title,
+      description,
+      date,
+      time,
+      is_on_diet: isOnDiet,
+      updated_at: new Date().toISOString(),
+    })
+
+    return reply.status(201).send()
+  })
 }
